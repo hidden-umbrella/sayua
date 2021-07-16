55dd0b74cb4fa568eab67617fdb01b40a83e17b6
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-16 13:54:50 +1000

    Added tests for the overlay endpoint
    I also fixed the test errors

--- tests/Images Tests/pat.test.ts
+++ tests/Images Tests/pat.test.ts
@@ -2,6 +2,6 @@ import * as sayua from '../../src/index'
 
 describe('Images - Pat', () => {
 	test('Should return a string / url', async () => {
-		expect(typeof await sayua.canvas.pat()).toBe('string')
+		expect(typeof await sayua.img.pat()).toBe('string')
 	})
 })
--- tests/Overlay tests/glass.test.ts
+++ tests/Overlay tests/glass.test.ts
@@ -2,8 +2,8 @@ import * as sayua from '../../src/index'
 
 let avatar = 'https://cdn.discordapp.com/avatars/389252140184633363/29dea926e406d3a8a754f0ce8ad4c4e0.png?size=2048'
 
-describe('Images - Gay', () => {
+describe('Images - Glass', () => {
 	test('Should return a string / url', async () => {
-		expect(typeof await sayua.canvas.gay(avatar)).toBe('string')
+		expect(typeof await sayua.canvas.glass(avatar)).toBe('string')
 	})
 })
