b2ef0b58bdc345b562d7f620443013074edb56dc
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-16 13:55:37 +1000

    Forgot to push the changes for this

--- tests/Overlay tests/glass.test.ts
+++ tests/Overlay tests/glass.test.ts
@@ -2,7 +2,7 @@ import * as sayua from '../../src/index'
 
 let avatar = 'https://cdn.discordapp.com/avatars/389252140184633363/29dea926e406d3a8a754f0ce8ad4c4e0.png?size=2048'
 
-describe('Images - Glass', () => {
+describe('Overlay - Glass', () => {
 	test('Should return a string / url', async () => {
 		expect(typeof await sayua.canvas.glass(avatar)).toBe('string')
 	})
